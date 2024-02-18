58a1bbc1e27d6fd5023fdefade99273bf2515bb3
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-02-18 21:34:47 +0000

    commented through js code more clearly

--- main.js
+++ main.js
@@ -22,12 +22,18 @@ let response = [
     "Very doubtful",
   ];
 
+// Function to go into the array and retrieve a response
   let showResponse = function()
   {
+// Creates random index that matches to answer in array
     let index = Math.floor(Math.random() * response.length);
+// Goes into array and retrieves the corresponding response    
     let answer = response[index];
-    let circle = document.getElementById( 'circle' );
+// Retrieves the HTML element that will contain the response text    
+    let circle = document.getElementById( 'eightBallResponses' );
+// Displays the background for the response by changing display CSS to none   
     circle.style.display = 'inline-block';
+// Displays the text response from the eight ball   
     circle.innerHTML = '<br><br>' + answer;
   }
   
